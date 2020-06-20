fdeb0dd03269fda0eb4738576709ec7c130bed87
Russkazkin <ruslan@skazkin.su>
2020-06-20 08:50:04 +0300

    Add final code optimization

--- index.php
+++ index.php
@@ -1,32 +1,30 @@
 <?php
 
-use app\engine\Db;
 use app\engine\Email;
 
 require_once 'vendor/autoload.php';
 
 $json = file_get_contents("php://input");
 $data = json_decode($json);
-if(!$data->email) {
+
+if(empty($data->email)) {
     die('0 emails received');
 }
+
 $email = new Email($data->email);
+
+$response = [
+    'count' => $email->checkUniqueness(),
+    'email' => $email->email(),
+];
+
 if($email->save()) {
-    $data = json_encode([
-        'count' => $email->checkUniqueness(),
-        'email' => $email->email(),
-        'result' => 'Email saved successfully'
-    ]);
+    $response['result'] = 'Email saved successfully';
     header("HTTP/1.0 200 Operation successful");
 } else {
-    $data = json_encode([
-        'count' => $email->checkUniqueness(),
-        'email' => $email->email(),
-        'result' => 'Email not saved'
-    ]);
+    $response['result'] = 'Email not saved';
     header("HTTP/1.0 400 Bad Request");
 }
 
-//header("Content-Type: application/json");
-$response = json_encode($data);
-echo $response;
+header("Content-Type: application/json");
+echo json_encode($response);
